bb3c3e3558816ef13928547cad8b4078bf9dcf02
mathyasp <mathyastp@gmail.com>
2024-02-08 11:42:26 -0500

    completed Authors and Genres forms, routes, and templates

--- books_app/forms.py
+++ books_app/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, DateField, SelectField, SubmitField, TextAreaFi
 from wtforms.ext.sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
 from wtforms.validators import DataRequired, Length, ValidationError
 from books_app.models import Audience, Book, Author, Genre
+import re
 
 class BookForm(FlaskForm):
     """Form to create a book."""
@@ -42,8 +43,21 @@ class AuthorForm(FlaskForm):
     # STRETCH CHALLENGE: Add more fields here as well as in `models.py` to
     # collect more information about the author, such as their birth date,
     # country, etc.
+    def validate_name(form, field):
+        if not re.match(r'^[A-Za-z\s]+$', field.data):
+            raise ValidationError('Name can only contain letters and spaces')
 
+    def validate_biography(form, field):
+        if not re.match(r'^[A-Za-z0-9\s\.,!?]+$', field.data):
+            raise ValidationError('Biography can only contain letters, numbers, spaces, and punctuation')
 
+    def validate_birth_date(form, field):
+        if not re.match(r'^[0-9]{4}-[0-9]{2}-[0-9]{2}$', field.data):
+            raise ValidationError('Birth date must be in the format YYYY-MM-DD')
+
+    def validate_country(form, field):
+        if not re.match(r'^[A-Za-z\s]+$', field.data):
+            raise ValidationError('Name can only contain letters and spaces')
 
 class GenreForm(FlaskForm):
     """Form to create a genre."""
@@ -51,4 +65,13 @@ class GenreForm(FlaskForm):
     # TODO: Fill out the fields in this class for:
     # - the genre's name (e.g. fiction, non-fiction, etc)
     # - a submit button
-    pass
+    name = StringField('Genre Name',
+        validators=[
+            DataRequired(),
+            Length(min=3, max=80, message="Your message needs to be betweeen 3 and 80 chars")
+        ])
+    submit = SubmitField('Submit')
+
+    def validate_name(form, field):
+        if not re.match(r'^[A-Za-z\s]+$', field.data):
+            raise ValidationError('Name can only contain letters and spaces')
